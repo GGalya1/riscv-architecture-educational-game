17244f9742b6fc551f0a8a82b5eaa00c6b4a46c5
GGalya1 <garturmail@gmail.com>
2026-06-22 20:49:30 +0200

    hotfix: commit achievements keys, because it's doesn't comprimize google console data

---
+++ Assets/Scripts/AchievementConstants.cs
@@ -0,0 +1,8 @@
+public static class AchievementConstants
+{
+    public const string FirstSteps = "";
+    public const string AllLevelsComplete = "";
+    public const string PerfectFirstTry = "";
+    public const string ThreeStarSolution = "";
+    public const string SolutionCounter = "";
+}
